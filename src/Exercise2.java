409d6f2f12fe816c3e5483e5ca5f03beac6e58df
Mohammed Faiz <mm2046@hw.ac.uk>
2023-08-17 21:50:48 +0400

    Ex 2 complete

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -7,8 +7,39 @@ import java.util.Scanner;
 public class Exercise2 {
 
 	public static void main(String[] args) {
-		/* First, copy across the code in the main method
-		   from your solution to Exercise 1. */
+		// Second order Equation ax^2 + bx + c = 0
+		double a = 0;
+		double b = 0;
+		double c = 0;
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter value for a");
+		a = scan.nextDouble();
+		
+		System.out.println("Enter value for b");
+		b = scan.nextDouble();
+		
+		System.out.println("Enter value for c");
+		c = scan.nextDouble();
+		
+		double delta = (b*b) - (4*a*c);
+		System.out.println("Delta = " + delta);
+		
+		if(delta < 0) {
+			System.out.println("Delta is negative, the equation has no real roots !!");
+			
+		}else if(delta == 0) {
+			double x = -b/(2*a);
+			System.out.println("Delta is Zero, the equation has 1 real root");
+			System.out.println("X1 = X2 = " + x);
+			
+		}else {
+			double x1 = (-b - Math.sqrt(delta)) / (2 * a); //x1 = (-b - √𝑑𝑒𝑙𝑡𝑎 )/(2*a)
+			double x2 = (-b + Math.sqrt(delta)) / (2 * a); //
+
+			System.out.println("Delta is Positive, the equation has 2 real roots");
+			System.out.println(" X1 = " + x1 + "\n X2 = " + x2); //x2 = (-b + √𝑑𝑒𝑙𝑡𝑎 )/(2*a)
+		}
 		
 	}
 
